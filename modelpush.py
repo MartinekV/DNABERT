054631e351e682beb4e346d073d959f531c51814
MartinekV <martinekvlastimil95@gmail.com>
2022-01-22 11:06:22 +0000

    Add tokenizer pushing

--- modelpush.py
+++ modelpush.py
@@ -1,11 +1,15 @@
-from transformers import AutoModel
+from transformers import AutoModel, AutoTokenizer
 
 #To push a model, you need to install git lfs and have a separate environment for huggingface 4.15 (newest)
 # sudo apt-get install git-lfs
 
-model = AutoModel.from_pretrained('CDNA_bert_6')
+model = AutoModel.from_pretrained('vlasta_output_6')
+tokenizer = AutoTokenizer.from_pretrained("vlasta_output_6")
+
 
 model.push_to_hub("Vlasta/CDNA_bert_6")
+tokenizer.push_to_hub("Vlasta/CDNA_bert_6")
+
 
 # Usage
 # model = AutoModel.from_pretrained('Vlasta/CDNA_bert_6')
